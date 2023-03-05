6a64244c70e39dc6a78b4d23f4900875f74bf4ba
Ravin G <100956485+Ravin01@users.noreply.github.com>
2023-03-05 07:38:22 +0530

    Add files via upload

---
+++ Day-3/resume.js
@@ -0,0 +1,21 @@
+// My resume in JSON format
+let myResume = {
+    name : 'Ravin G',
+    mobile : 8870290307,
+    email : 'ravinganapathi@gmail.com',
+    objective : 'i seek challenging opportunities where I can fully use my skills for the success of the organization',
+    education : [
+        ['SSLC(2015-2017) - 83%'],
+        ['HSC(2017-2019) - 81%'],
+        ['B.E(2019-2023) - 8.1']
+    ],
+    skills : {
+    software : ['Html','css','Bootstrap','Javascript','React','Java','mySQL','c','python'],
+    hardware : ['PCB','Arduino']
+    },
+    projects : ['Portfolio website','obstacle avoid robot','smart farm'],
+    achievements : ['discipline','sports','PCB'],
+    interest : ['hardwork','find a solution','sports','listening music','driving','photo and video editing','organizer','volunteer'],
+    language : ['Tamil','English',]
+};
+console.log(myResume);
